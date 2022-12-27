4a9b1457a7ddda30434dc439e3e112c1435e8056
padarcs <padar.csaba.tibor@gmail.com>
2022-12-27 19:01:39 +0100

    Add basic logic login and signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
     constructor(private prisma: PrismaService) { }
-    signup() {
-        return { msg: 'I have signed up' };
+    async signup(dto: AuthDto) {
+        const hash = await argon.hash(dto.password);
+        const user = await this.prisma.user.create({
+            data: {
+                email: dto.email,
+                hash: hash,
+                firstName: 'John',
+                lastName: 'Doe'
+            }
+        })
+        delete user.hash;
+        return user;
     }
 
-    signin() {
-        return { msg: 'I have signed in' };
+    async signin(dto: AuthDto) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email
+            }
+        });
+        if (!user) {
+            throw new ForbiddenException('User doesn not exists!')
+        }
+        const pwMatch = await argon.verify(user.hash, dto.password);
+        if (!pwMatch) {
+            throw new ForbiddenException('Wrong password!')
+        }
+        delete user.hash;
+        return user;
     }
 }
