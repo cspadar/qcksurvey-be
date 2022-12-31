862eb1fd0ec4f932323013efa704b75ade81ac09
padarcs <padar.csaba.tibor@gmail.com>
2022-12-31 16:13:42 +0100

    add nest jwt, jwt passport, signin token, jwt strategy, secret

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,17 @@ import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService) { }
+    constructor(
+        private prisma: PrismaService,
+        private jwt: JwtService,
+        private config: ConfigService
+    ) { }
+
     async signup(dto: AuthDto) {
         const hash = await argon.hash(dto.password);
         const user = await this.prisma.user.create({
@@ -16,8 +23,7 @@ export class AuthService {
                 lastName: 'Doe'
             }
         })
-        delete user.hash;
-        return user;
+        return this.signToken(user.id, user.email);
     }
 
     async signin(dto: AuthDto) {
@@ -33,7 +39,23 @@ export class AuthService {
         if (!pwMatch) {
             throw new ForbiddenException('Wrong password!')
         }
-        delete user.hash;
-        return user;
+
+        return this.signToken(user.id, user.email);
+    }
+
+    async signToken(userId: string, email: string): Promise<{ access_token: string }> {
+        const payload = {
+            sub: userId,
+            email
+        }
+
+        const token = await this.jwt.signAsync(payload, {
+            expiresIn: '15m',
+            secret: this.config.get('JWT_SECRET')
+        });
+
+        return {
+            access_token: token
+        }
     }
 }
