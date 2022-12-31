2000479ba3a0a5fa7b1d809acf3ba610869ba2bd
padarcs <padar.csaba.tibor@gmail.com>
2022-12-31 15:35:12 +0100

    add config module, db url from env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    PrismaModule,
+    AuthModule,
+    UserModule
+  ],
 })
-export class AppModule {}
+export class AppModule { }
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor() {
+    constructor(config: ConfigService) {
         super({
             datasources: {
                 db: {
-                    url: 'THIS_SHOULD_COME_FROM_ENV_FILE'
+                    url: config.get('DATABASE_URL')
                 }
             }
         })
