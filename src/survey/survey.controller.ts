398b699f575210717e4a3b64ac1f7f03323d74d6
padarcs <padar.csaba.tibor@gmail.com>
2023-01-09 18:54:01 +0100

    Add user decorator for req.user

--- src/survey/survey.controller.ts
+++ src/survey/survey.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } fro
 import { SurveyService } from './survey.service';
 import { CreateSurveyDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
+import { User } from 'src/decorators/user.decorator';
 
 @UseGuards(AuthGuard("jwt"))
 @Controller('survey')
@@ -9,22 +10,22 @@ export class SurveyController {
   constructor(private readonly surveyService: SurveyService) { }
 
   @Post()
-  create(@Body() createSurveyDto: CreateSurveyDto, @Req() req) {
-    return this.surveyService.create(createSurveyDto, req.user.id);
+  create(@Body() createSurveyDto: CreateSurveyDto, @User() user) {
+    return this.surveyService.create(createSurveyDto, user.id);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string, @Req() req) {
-    return this.surveyService.findOne(id, req.user.id);
+  findOne(@Param('id') id: string, @User() user) {
+    return this.surveyService.findOne(id, user.id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSurveyDto: Partial<CreateSurveyDto>, @Req() req) {
-    return this.surveyService.update(id, updateSurveyDto, req.user.id);
+  update(@Param('id') id: string, @Body() updateSurveyDto: Partial<CreateSurveyDto>, @User() user) {
+    return this.surveyService.update(id, updateSurveyDto, user.id);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string, @Req() req) {
-    return this.surveyService.remove(id, req.user.id);
+  remove(@Param('id') id: string, @User() user) {
+    return this.surveyService.remove(id, user.id);
   }
 }
